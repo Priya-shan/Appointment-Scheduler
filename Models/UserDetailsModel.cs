69354969bf1f5b9d34fa96f5b5f1fa8fd7eb594d
priya-shan <shanmugapriyashanu2002@gmail.com>
2023-04-15 00:07:34 +0530

    Added UserDetails Model

---
+++ Models/UserDetailsModel.cs
@@ -0,0 +1,22 @@
+﻿using MessagePack;
+using Microsoft.Build.Framework;
+
+using System.ComponentModel.DataAnnotations;
+namespace Appointment_Scheduler.Models
+{
+    public class UserDetailsModel
+    {
+        //public int userid { get; set; }
+        [System.ComponentModel.DataAnnotations.Key]
+        [System.ComponentModel.DataAnnotations.Required]
+        public string email { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public string username { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public string password { get; set; }
+
+    }
+
+}
