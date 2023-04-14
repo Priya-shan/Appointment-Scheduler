ffd80ad946ce312aa19f1a27d03463705a789b03
priya-shan <shanmugapriyashanu2002@gmail.com>
2023-04-15 00:16:08 +0530

    Added AppointmentDetails DB Context

---
+++ Data/AppointmentDetailsDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Appointment_Scheduler.Models;
+
+namespace Appointment_Scheduler.Data
+{
+    public class AppointmentDetailsDBContext : DbContext
+    {
+        public AppointmentDetailsDBContext(DbContextOptions<AppointmentDetailsDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AppointmentDetailsModel> AppointmentDetails { get; set; }
+    }
+}
