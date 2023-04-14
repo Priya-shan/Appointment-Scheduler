74702b38b6e8e1a8b834ec896098e1a960176057
priya-shan <shanmugapriyashanu2002@gmail.com>
2023-04-15 00:21:15 +0530

    List all Appointments Functionality added

--- Controllers/AppointmentsController.cs
+++ Controllers/AppointmentsController.cs
@@ -7,6 +7,29 @@ namespace Appointment_Scheduler.Controllers
 {
     public class AppointmentsController : Controller
     {
+        readonly AppointmentDetailsDBContext db;
+        public AppointmentsController(AppointmentDetailsDBContext dbContext)
+        {
+            db = dbContext;
+        }
         
+        public List<AppointmentDetailsModel> getAppointments()
+        {
+            List<AppointmentDetailsModel> appointmentList= new List<AppointmentDetailsModel>();
+            appointmentList = db.AppointmentDetails.ToList();
+            foreach(var item in appointmentList)
+            {
+                Console.WriteLine("email :"+item.email);
+            }
+            return appointmentList;
+        }
+        
+        // GET: AppointmentsController
+        public ActionResult Index()
+        {
+            return View(getAppointments());
+        }
+        
+
     }
 }
