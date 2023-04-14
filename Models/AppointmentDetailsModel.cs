3680c02ca3feb60aa372a9a2e63e35815263e428
priya-shan <shanmugapriyashanu2002@gmail.com>
2023-04-15 00:15:37 +0530

    Appointment Details Model Added

---
+++ Models/AppointmentDetailsModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Appointment_Scheduler.Models
+{
+    public class AppointmentDetailsModel
+    {
+        [Key]
+        [Required]
+        public int appointment_id { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public TimeSpan start_time { get; set; }
+        [Required]
+        public TimeSpan end_time { get; set; }
+        [Required]
+        public string duration { get; set; }
+        [Required]
+        public DateTime date { get; set; }
+        [Required]
+        public string description { get; set; }
+        [Required]
+        public string status { get; set; }
+    }
+}
