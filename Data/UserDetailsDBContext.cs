b13b7cc0414717f445cab0c90e4bcd62d8cdb46c
priya-shan <shanmugapriyashanu2002@gmail.com>
2023-04-15 00:08:04 +0530

    UserDetails DB Context Added

---
+++ Data/UserDetailsDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Appointment_Scheduler.Models;
+
+namespace Appointment_Scheduler.Data
+{
+    public class UserDetailsDBContext : DbContext
+    {
+        public UserDetailsDBContext(DbContextOptions<UserDetailsDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserDetailsModel> UserDetails { get; set; }
+    }
+}
