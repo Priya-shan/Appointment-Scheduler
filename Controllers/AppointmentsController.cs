a86c51d277f54092ee027999612777ff41071ace
priya-shan <shanmugapriyashanu2002@gmail.com>
2023-04-15 00:18:33 +0530

    Added Appointment Controller

---
+++ Controllers/AppointmentsController.cs
@@ -0,0 +1,12 @@
+﻿using Appointment_Scheduler.Data;
+using Appointment_Scheduler.Models;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Plugins;
+
+namespace Appointment_Scheduler.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        
+    }
+}
